eb17c45b9e4ff074f3598578abd400ac9c0a8a53
Srivathsan Ram <sri5617@gmail.com>
2026-08-21 15:44:55 +0800

    adding basic echo

--- src/main/java/Floppy.java
+++ src/main/java/Floppy.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 /**
- * Starts the Floppy chatbot.
+ * Starts the Floppy chatbot and echoes commands until the user exits.
  */
 public class Floppy {
+    /**
+     * Runs the chatbot's command loop.
+     *
+     * @param args command-line arguments; not used
+     */
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = " _____ _                         \n"
                 + "|  ___| | ___  _ __  _ __  _   _\n"
                 + "| |_  | |/ _ \\| '_ \\| '_ \\| | | |\n"
@@ -11,6 +19,23 @@ public class Floppy {
                 + "               |_|   |_|    |___/ \n";
         System.out.println(banner);
         System.out.println("Hello! I'm Floppy.");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(divider);
+
+            if (command.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(divider);
+        }
+        scanner.close();
     }
 }
