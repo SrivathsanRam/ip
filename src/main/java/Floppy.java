240ef17d8b9c15de88bf549de12eb470c2fa5238
Srivathsan Ram <sri5617@gmail.com>
2026-08-21 15:26:33 +0800

    changing all instances of Duke to Floppy

---
+++ src/main/java/Floppy.java
@@ -0,0 +1,11 @@
+public class Floppy {
+    public static void main(String[] args) {
+        String banner = " _____ _                         \n"
+                + "|  ___| | ___  _ __  _ __  _   _\n"
+                + "| |_  | |/ _ \\| '_ \\| '_ \\| | | |\n"
+                + "|  _| | | (_) | |_) | |_) | |_| |\n"
+                + "|_|   |_|\\___/| .__/| .__/ \\__, |\n"
+                + "               |_|   |_|    |___/ \n";
+        System.out.println(banner);
+    }
+}
