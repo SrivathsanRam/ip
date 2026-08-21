056fafa25d8139c22d889d8f2ef03b7860ffae7f
Srivathsan Ram <sri5617@gmail.com>
2026-08-21 15:31:34 +0800

    basic greeting

--- src/main/java/Floppy.java
+++ src/main/java/Floppy.java
@@ -1,3 +1,6 @@
+/**
+ * Starts the Floppy chatbot.
+ */
 public class Floppy {
     public static void main(String[] args) {
         String banner = " _____ _                         \n"
@@ -7,5 +10,7 @@ public class Floppy {
                 + "|_|   |_|\\___/| .__/| .__/ \\__, |\n"
                 + "               |_|   |_|    |___/ \n";
         System.out.println(banner);
+        System.out.println("Hello! I'm Floppy.");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
